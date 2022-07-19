08782a345cfefd4f786db551d79a832c6b055318
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-07-19 21:20:24 +0530

    create basic login form

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react'
-import { TextField, Button, Paper, Grid } from '@mui/material';
+import { TextField, Button, Paper, Grid, Typography } from '@mui/material';
 
 const Login: FC = () => {
     return (
-        <Grid container direction="column" justifyContent={'center'} alignItems={"center"}>
-            <Paper style={{ width: "40%", padding: "20px" }}>
+        <Grid sx={{ margin: "1rem 0", alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Typography variant="h3" gutterBottom component="div">
+                Login
+            </Typography>
+            <Paper sx={{ margin: "1rem 20rem", padding: "3rem 1.5rem", backgroundColor: "#e3dddd", borderRadius: "10px", width: "50%", }}>
                 <Grid container direction="column" justifyContent={'center'} alignItems={"center"} spacing={3}>
                     <Grid item xs={6}>
                         <TextField label="Username"></TextField>
