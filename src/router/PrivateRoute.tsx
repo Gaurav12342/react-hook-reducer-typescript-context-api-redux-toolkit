2e2b56254d9554a2db0d323530706118999813d2
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-07-28 00:24:29 +0530

    Added basic header

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
 import { Navigate, useLocation, Outlet } from "react-router-dom";
+import Header from "../components/layout/Header";
 import { isLogin } from '../utils/auth';
 
 const PrivateOutlet: FC = () => {
     const { pathname } = useLocation();
 
     return isLogin ? (
-        <Outlet />
+        <>
+            <Header />
+            <Outlet />
+        </>
     ) : (
         <Navigate to="/" state={{ from: pathname }} replace />
     );
