ec48d8d5f3aadf19fa0ce88a50b9ee6b5ad745a7
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-07-28 22:21:13 +0530

    Create axios interceptor

---
+++ src/utils/AxiosInterceptor.tsx
@@ -0,0 +1,27 @@
+import { isLogin } from "./auth";
+import axios from "axios";
+
+const isLogged = JSON.parse(isLogin)
+axios.defaults.baseURL = "http://restapi.adequateshop.com/api/";
+axios.defaults.headers.common["Authorization"] = `Bearer ${isLogged.Token}`;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+    (request) => {
+        return request;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axios;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react'
 import { useSelector } from 'react-redux';
 import { loginUser } from '../../store/login/loginSlice';
+import { isLogin } from '../../utils/auth';
 
 const Home: FC = () => {
     const user = useSelector(loginUser);
+    const logged = JSON.parse(isLogin)
+    console.log("isLogin", logged.Token);
 
-    console.log("user", user);
 
     return (
         <div>index1212</div>
