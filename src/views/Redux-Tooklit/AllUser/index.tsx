441834178809f3617ff43e26279a90f9ce36c726
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-07-30 13:10:26 +0530

    added loader in table

--- src/views/Redux-Tooklit/AllUser/index.tsx
+++ src/views/Redux-Tooklit/AllUser/index.tsx
@@ -8,33 +8,14 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUsers, usersData, userLoading } from '../../../store/user/getAllUserSlice';
-
-
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 
 const AllUsers: FC = () => {
     const user = useSelector(usersData);
     const userLogin = useSelector(userLoading);
-    console.log("userData =>", user);
-    console.log("userLogin =>", userLogin);
-
     const dispatch = useDispatch();
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
@@ -43,29 +24,36 @@ const AllUsers: FC = () => {
         <div>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>User Id</TableCell>
-                            <TableCell align="right">Name</TableCell>
-                            <TableCell align="right">Email</TableCell>
-                            <TableCell align="right">Location</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {user?.data?.map((row: any) => (
-                            <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.id}
-                                </TableCell>
-                                <TableCell align="right">{row.name}</TableCell>
-                                <TableCell align="right">{row.email}</TableCell>
-                                <TableCell align="right">{row.location}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
+                    {userLogin ?
+                        <Box sx={{ width: "100%" }}> <Skeleton />
+                            <Skeleton animation="wave" />
+                            <Skeleton animation={false} />
+                        </Box>
+                        : <>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>User Id</TableCell>
+                                    <TableCell align="right">Name</TableCell>
+                                    <TableCell align="right">Email</TableCell>
+                                    <TableCell align="right">Location</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {user?.data?.map((row: any) => (
+                                    <TableRow
+                                        key={row.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.id}
+                                        </TableCell>
+                                        <TableCell align="right">{row.name}</TableCell>
+                                        <TableCell align="right">{row.email}</TableCell>
+                                        <TableCell align="right">{row.location}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </>}
                 </Table>
             </TableContainer>
         </div>
